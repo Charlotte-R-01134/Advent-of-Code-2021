fa2f94f2229e0990887bf43f004214d013457e35
Charlotte-R <49107073+Charlotte-R-01134@users.noreply.github.com>
2021-12-04 21:46:53 +0000

    Day 4 part 1

--- Day 4/day 4.py
+++ Day 4/day 4.py
@@ -1,52 +1,68 @@
 
 def getInput():
+    """
+    gets inputs from start of txt file
+    """
     return [line.rstrip() for line in open("Day 4\day 4.txt").readline().rstrip().split(",")]
 
 def getCards():
+    """
+    Sorts each card into a matrix
+    """
     with open("Day 4\day 4.txt") as f:
         lines = f.readlines()[2:]
         Matrix = []
-        table = []
+        card = []
         for line in lines:
             if line == "\n":
-                Matrix.append(table)
-                table = []
                 continue
-            numbers = []
             row = line.rstrip().split(" ")
             for num in row:
                 if num == "":
                     row.pop(row.index(num))
-            numbers.append(row)
-            table.append(numbers)
-        Matrix.append(table)
+            card.append(row)
+            if len(card) == 5:
+                    Matrix.append(card)
+                    card = []
+                    continue
     return Matrix
 
 def markSelected(Matrix, input):
+    """
+    Search for input in Matrix
+    """
     for card in Matrix:
-        for table in card:
-            for row in table:
+            for row in card:
                 for num in row:
                     if num == input:
-                        Matrix[Matrix.index(card)][card.index(table)][table.index(row)][row.index(num)] = ""
+                        Matrix[Matrix.index(card)][card.index(row)][row.index(num)] = ""
     return Matrix
 
 def calculateScore(card, input):
     score = 0
-    for table in card:
-        for row in table:
-            for num in row:
-                if num != "":
-                    score += int(num)
+    for row in card:
+        for num in row:
+            if num != "":
+                score += int(num)
     print(score*int(input))
 
 def checkIfBingo(Matrix, input):
+    """
+    Searches row and column for full empty row or column
+    """
     for card in Matrix:
-        for table in card:
-            for row in table:
-                x = row.count("")
-                if x == len(row):
-                    print("BINGO!")
+        for row in range(5):
+            for col in range(5):
+                if card[row][col] == "":
+                    x = card[row].count("")
+                    if x == len(card[row]):
+                        print("Bingo!")
+                        calculateScore(card, input)
+                        return True
+            if card[col][row] == "":
+                x = card[col].count("")
+                if x == len(card[col]):
+                    print("Bingo!")
                     calculateScore(card, input)
                     return True
 
